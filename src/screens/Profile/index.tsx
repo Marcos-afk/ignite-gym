17f97aa907e47f7b0838a9c9db713a5ba90c80cc
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-03-06 01:32:46 -0300

    :sparkles: add profile screen

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,51 @@
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
+import { PHOTO_SIZE } from './constants';
+import { TouchableOpacity } from 'react-native';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 export const Profile = () => {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton w={PHOTO_SIZE} h={PHOTO_SIZE} rounded="full" startColor="gray.500" endColor="gray.400" />
+          ) : (
+            <UserPhoto
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/72817154?v=4',
+              }}
+              alt="avatar"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input placeholder="Nome" bg="gray.600" />
+          <Input placeholder="Email" bg="gray.600" isDisabled />
+
+          <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12}>
+            Alterar senha
+          </Heading>
+
+          <Input placeholder="Senha antiga" bg="gray.600" secureTextEntry />
+          <Input placeholder="Nova senha" bg="gray.600" secureTextEntry />
+          <Input placeholder="Confirmar senha" bg="gray.600" secureTextEntry />
+          <Button title="Atualizar" mt={4} />
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 };

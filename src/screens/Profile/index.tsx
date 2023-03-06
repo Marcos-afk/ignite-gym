1d2caf123065cc654e87c77bb369f59af8e96c05
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-03-06 07:49:23 -0300

    :sparkles: add user photo picker

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,14 +1,70 @@
 import { useState } from 'react';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
 import { PHOTO_SIZE } from './constants';
 import { TouchableOpacity } from 'react-native';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 export const Profile = () => {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://avatars.githubusercontent.com/u/72817154?v=4');
+  const toast = useToast();
+
+  const handleUserPhotoSelect = async () => {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+        allowsMultipleSelection: false,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const [image] = photoSelected.assets;
+      if (image.uri) {
+        const photoInfo = await FileSystem.getInfoAsync(image.uri);
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          toast.show({
+            title: 'Erro ao selecionar foto de perfil',
+            description: 'A foto selecionada deve ter no máximo 5MB',
+            placement: 'top',
+            bgColor: 'red.500',
+          });
+          return;
+        }
+
+        setUserPhoto(image.uri);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.show({
+          title: 'Erro ao selecionar foto de perfil',
+          description: error.message,
+          placement: 'top',
+          bgColor: 'red.500',
+        });
+        return;
+      }
+
+      toast.show({
+        title: 'Erro ao selecionar foto de perfil',
+        description: 'Ocorreu um erro ao selecionar a foto de perfil',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  };
 
   return (
     <VStack flex={1}>
@@ -20,14 +76,14 @@ export const Profile = () => {
           ) : (
             <UserPhoto
               source={{
-                uri: 'https://avatars.githubusercontent.com/u/72817154?v=4',
+                uri: userPhoto,
               }}
               alt="avatar"
               size={PHOTO_SIZE}
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
               Alterar foto
             </Text>

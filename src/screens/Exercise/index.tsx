976496d3335ca6ddb08495d94f74de334810a9cc
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-03-06 06:37:03 -0300

    :recycle: interface tweaks

--- src/screens/Exercise/index.tsx
+++ src/screens/Exercise/index.tsx
@@ -16,26 +16,25 @@ export const Exercise = () => {
   };
 
   return (
-    <ScrollView>
-      <VStack flex={1}>
-        <VStack px={8} bg="gray.600" pt={12}>
-          <TouchableOpacity onPress={handleGoBack}>
-            <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
-          </TouchableOpacity>
-          <HStack justifyContent="space-between" mt={4} mb={8} alignItems="center">
-            <Heading color="gray.100" fontSize="lg" flexShrink={1}>
-              Puxada frontal
-            </Heading>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+        <HStack justifyContent="space-between" mt={4} mb={8} alignItems="center">
+          <Heading color="gray.100" fontSize="lg" flexShrink={1}>
+            Puxada frontal
+          </Heading>
 
-            <HStack alignItems="center">
-              <BodySvg />
-              <Text color="gray.200" ml={1} textTransform="capitalize">
-                Costas
-              </Text>
-            </HStack>
+          <HStack alignItems="center">
+            <BodySvg />
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
           </HStack>
-        </VStack>
-
+        </HStack>
+      </VStack>
+      <ScrollView>
         <VStack p={8}>
           <Image
             source={ExerciseImg}
@@ -66,7 +65,7 @@ export const Exercise = () => {
             <Button title="Marcar como realizado" />
           </Box>
         </VStack>
-      </VStack>
-    </ScrollView>
+      </ScrollView>
+    </VStack>
   );
 };
